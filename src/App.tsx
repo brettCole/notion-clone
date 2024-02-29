b90766a22532406ebf9db4be64720fd2d3389859
brett.cole <acodercalledcole@gmail.com>
2024-02-29 10:28:32 -0500

    added react router with auth provider for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
-import './App.css'
-import { Page } from './Page/Page'
-import { AppStateProvider } from './state/AppStateContext'
-import { createPage } from './utils/createPage'
+import './App.css';
+import { Page } from './Page/Page';
+import { AppStateProvider } from './state/AppStateContext';
+import { createPage } from './utils/createPage';
+import { Route, Routes } from 'react-router-dom';
+import { Auth } from './auth/Auth';
+import { Private } from './auth/Private';
 
 const initialState = createPage();
 
 function App() {
 
   return (
-    <AppStateProvider initialState={initialState}>
-      <Page />
-    </AppStateProvider>
+    <Routes>
+      <Route path='/auth' element={<Auth />} />
+      <Route 
+        path='/:id' 
+        element={
+          <Private component={
+            <AppStateProvider initialState={initialState}>
+              <Page />
+            </AppStateProvider>
+          } />
+        } 
+      />
+      <Route 
+        path='/' 
+        element={
+          <Private component={
+            <AppStateProvider initialState={initialState}>
+              <Page />
+            </AppStateProvider>
+          } />
+        }
+      />
+    </Routes>
   )
 }
 
--- src/Page/Page.tsx
+++ src/Page/Page.tsx
@@ -1,31 +1,45 @@
 import { useFocusedNodeIndex } from "./useFocusedNodeIndex";
 import { Cover } from "./Cover";
 import { Spacer } from "./Spacer";
-import { BasicNode } from "../Node/BasicNode";
+import { NodeContainer } from "../Node/NodeContainer";
 import { Title } from "./Title";
 import { nanoid } from "nanoid";
 import { useAppState } from "../state/AppStateContext";
+import { DndContext, DragOverlay, DragEndEvent } from "@dnd-kit/core";
+import { SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
 
 export const Page = () => {
-  const { title, nodes, addNode, setTitle } = useAppState();
+  const { title, nodes, addNode, reorderNodes, setTitle } = useAppState();
   const [focusedNodeIndex, setFocusedNodeIndex] = useFocusedNodeIndex({
     nodes
   });
+
+  const handleDragEvent = (event: DragEndEvent) => {
+    const { active, over } = event;
+    if (over?.id && active.id !== over?.id) {
+      reorderNodes(active.id as string, over.id as string);
+    }
+  }
 
   return (
     <>
       <Cover />
       <div>
         <Title addNode={addNode} title={title} changePageTitle={setTitle} />
-        {nodes.map((node, index) => (
-          <BasicNode
-            key={node.id}
-            node={node}
-            isFocused={focusedNodeIndex === index}
-            updateFocusedIndex={setFocusedNodeIndex}
-            index={index}
-          />
-        ))}
+        <DndContext onDragEnd={handleDragEvent}>
+          <SortableContext items={nodes} strategy={verticalListSortingStrategy}>
+            {nodes.map((node, index) => (
+              <NodeContainer
+                key={node.id}
+                node={node}
+                isFocused={focusedNodeIndex === index}
+                updateFocusedIndex={setFocusedNodeIndex}
+                index={index}
+              />
+            ))}
+          </SortableContext>
+          <DragOverlay />
+        </DndContext>
         <Spacer
         handleClick={() => {
           addNode({ type: "text", value: "", id: nanoid() }, nodes.length)
